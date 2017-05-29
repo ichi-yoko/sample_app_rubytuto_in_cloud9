80940fdfad46a2be56ae6e3fd347df67538bc6c3
ichi-yoko <yangzi1003dui@gmail.com>
2017-05-29 08:19:33 +0000

    Modify heroku host info for email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = 'yoko-rails-tutorial-sample-app.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
